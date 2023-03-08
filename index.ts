7a78128de63653296e15b39afef312c2dc4dd9d2
Dmytro Didukh <jeriko.outback@gmail.com>
2023-03-08 12:41:15 +0200

    env

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 // import helmet from 'helmet';
 // import morgan from 'morgan';
+import config from './config';
 
 dotenv.config();
 
 const app: Express = express();
+mongoose.connect(config.MONGO_URI);
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-    console.log(`SERVER IS STARTED ON ${port}`);
+app.listen(config.PORT, () => {
+    console.log(`SERVER IS STARTED ON ${config.PORT}`);
 });
