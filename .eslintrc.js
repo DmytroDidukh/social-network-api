2c05de80b78ef95f889561154684b68cefcc293f
Dmytro Didukh <jeriko.outback@gmail.com>
2023-03-08 16:50:57 +0200

    routes init

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint/eslint-plugin'],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+    ],
+    root: true,
+    env: {
+        node: true,
+        jest: true,
+    },
+    ignorePatterns: ['.eslintrc.js'],
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-plusplus': 'off',
+        'no-underscore-dangle': 'off',
+        'no-param-reassign': [
+            'error',
+            {
+                props: false,
+            },
+        ],
+        'linebreak-style': 'off',
+        'class-methods-use-this': 'off',
+        'max-len': [
+            'error',
+            {
+                code: 120,
+            },
+        ],
+        'object-curly-newline': 'off',
+        'import/prefer-default-export': 'off',
+        'import/no-default-import': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        'prettier/prettier': [
+            'warn',
+            {
+                endOfLine: 'auto'
+            }
+        ],
+        'spaced-comment': [
+            'error',
+            'always',
+            {
+                markers: ['/'],
+            },
+        ],
+    },
+};
--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 
-import config from './config/config';
+import config from 'config/config';
 
 import { rootRouter } from './routes';
 
