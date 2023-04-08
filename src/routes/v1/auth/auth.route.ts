9046f1fa2b55b9c8b11c21ed43b14c7a9dc536c8
Dmytro Didukh <jeriko.outback@gmail.com>
2023-04-08 17:10:17 +0300

    auth-route updates

--- src/routes/v1/auth/auth.route.ts
+++ src/routes/v1/auth/auth.route.ts
@@ -1,9 +1,21 @@
 import express, { Request, Response } from 'express';
 
+import { userValidator } from 'middleware/validators/user-validator';
+import { validate } from 'middleware/validate';
+import { authService } from 'services/auth';
+
 const router = express.Router();
 
 // REGISTER
-router.post('/register', (req: Request, res: Response) => {
-    res.send('HELLO');
-});
+router.post(
+    '/sign-up',
+    userValidator.registrationSchema,
+    validate,
+    async (req: Request, res: Response): Promise<void> => {
+        const newUser = await authService.signUp(req.body);
+
+        res.send(newUser);
+    },
+);
+
 export default router;
