e94c5c9cd047454d8b71a198d2897e6d13457173
Dmytro Didukh <jeriko.outback@gmail.com>
2023-04-09 11:53:29 +0300

    libs

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,6 @@ import { router as v1Router } from './v1';
 
 const rootRouter = express.Router();
 
-// TODO: change routes to 'auth/v1' instead of 'v1/auth'
 rootRouter.use('/v1', v1Router);
 
 export { rootRouter };
