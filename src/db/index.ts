3b721ace5ac1e1c2e54ac0b64e58237431381429
Dmytro Didukh <jeriko.outback@gmail.com>
2023-04-15 21:57:20 +0300

    sessions init

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,10 @@
 import mongoose from 'mongoose';
 
 import config from 'config/config';
-
+// TODO: Implement sessions
+// https://github.com/jdesboeufs/connect-mongo/blob/master/example/mongoose.js
+// https://www.npmjs.com/package/connect-mongo
+// https://www.youtube.com/watch?v=J1qXK66k1y4
 const setupDatabase = () => {
     return mongoose
         .connect(config.MONGO_URI)
