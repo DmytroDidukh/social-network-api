dc1b636ac13bd4a2a624ef5d8a3da53c8a443a3f
Dmytro Didukh <jeriko.outback@gmail.com>
2023-05-02 22:14:11 +0300

    auth sign-in controller

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -2,8 +2,9 @@ import { NextFunction, Request, Response } from 'express';
 import passport from 'passport';
 import { authService } from 'services/auth';
 import { createController } from 'middleware/controller';
-import { IUserDto } from 'types/interfaces/user';
-import { responseService } from 'services/response';
+import { IUserDto, IUserModel } from 'types/interfaces/user';
+import { userService } from 'services/user';
+import { ApiSignInCredentialsError } from 'api/error';
 
 const controller = createController();
 
@@ -11,59 +12,24 @@ function signUp(req: Request): Promise<IUserDto> {
     return authService.signUp(req.body);
 }
 
-function signIn(error, req, res) {
-    // handle error
-    console.log('ERROR: ', error);
-    return responseService.sendError(res, error);
-}
-function signIn2(req: Request, res: Response, next: NextFunction) {
-    // return passport.authenticate('local', function (err, user, info) {
-    //     if (err) {
-    //         return err;
-    //     }
-    //     if (user) {
-    //         //const token = user.generateJwt();
-    //         return user;
-    //     } else {
-    //         return info;
-    //     }
-    // })(req, res, next);
-    // const user = passport.authenticate(
-    //     'local',
-    //     {
-    //         successRedirect: '/v1/users/me',
-    //         // failureRedirect: '/v1/error',
-    //     },
-    //     (err, user, info) => {
-    //         console.log('CALLBACK');
-    //         console.log(user);
-    //         console.log(err);
-    //
-    //         return user;
-    //     },
-    // )(req, res, next);
-    //
-    // return user;
-
-    // console.log('USER@: ', user);
-    passport.authenticate('local', function (err, user) {
-        if (err) {
-            return next(err);
-        }
-        if (!user) {
-            return res.status(401).json({ message: 'Authentication failed' });
-        }
-        req.logIn(user, function (err) {
-            if (err) {
-                return next(err);
+async function signIn(req: Request, res: Response, next: NextFunction) {
+    return await new Promise((resolve, reject) => {
+        passport.authenticate('local', (err: ApiSignInCredentialsError, user: IUserModel) => {
+            if (err || !user) {
+                reject(err);
             }
-            return res.json({ message: 'Authentication successful' });
-        });
-    })(req, res, next);
+
+            req.logIn(user, (err) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(userService.mapModelToDto(user));
+            });
+        })(req, res, next);
+    });
 }
 
 export const authController = {
     signUp: controller(signUp),
     signIn: controller(signIn),
-    signIn2: signIn2,
 };
--- src/routes/v1/auth/auth.route.ts
+++ src/routes/v1/auth/auth.route.ts
@@ -2,8 +2,6 @@ import express from 'express';
 import { userValidator } from 'middleware/validators/user-validator';
 import { authController } from 'controllers/auth';
 import { validate } from 'middleware/validate';
-// import passport from 'passport';
-// import { responseService } from 'services/response';
 
 const router = express.Router();
 
@@ -11,24 +9,6 @@ const router = express.Router();
 router.post('/sign-up', userValidator.signUpSchema, validate, authController.signUp);
 
 // SIGN-IN
-router.post(
-    '/sign-in',
-    userValidator.signInSchema,
-    validate,
-    // passport.authenticate('local', {
-    //     successRedirect: '/v1/users/me',
-    //     // failureRedirect: '/v1/error',
-    // }),
-    // function (error, req, res, next) {
-    //     // handle error
-    //     console.log('ERROR: ', error);
-    //     return responseService.sendError(res, error);
-    // },
-    authController.signIn2,
-    // function (req, res) {
-    //     console.log(req.user);
-    //     res.redirect('/v1/users/me');
-    // },
-);
+router.post('/sign-in', userValidator.signInSchema, validate, authController.signIn);
 
 export default router;
